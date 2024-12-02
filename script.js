2ba9a03f1586bf54dd0784876e885984fbb5aadb
Abhiram K Rajan <abhirammain01@gmail.com>
2024-12-02 12:05:02 +0530

    Pushing index.html and script.js files

---
+++ script.js
@@ -0,0 +1,54 @@
+// Select the h1 element with the id of main-title and log it to the console.
+console.log("Select the h1 element with the id of main-title and log it to the console.");
+const main_title = document.getElementById("main-title");
+console.log(main_title);
+console.log("________________________________________________________________________________________________________");
+
+// Using getElementsByClassName: Select all elements with the class container and log them to the console.
+console.log("Using getElementsByClassName: Select all elements with the class container and log them to the console.");
+const container = document.getElementsByClassName("container");
+console.log(container);
+console.log("________________________________________________________________________________________________________");
+
+
+// Select all elements with the class section and log them to the console.
+console.log("Select all elements with the class section and log them to the console.");
+const section = document.getElementsByClassName("section");
+console.log(section);
+console.log("________________________________________________________________________________________________________");
+
+
+// Using getElementsByTagName: Select all p elements and log them to the console.
+console.log("Using getElementsByTagName: Select all p elements and log them to the console.");
+const p = document.getElementsByTagName("p");
+console.log(p);
+console.log("________________________________________________________________________________________________________");
+
+
+// Select the first button with the class btn and log it to the console.
+console.log("Select the first button with the class btn and log it to the console.");
+const firstBtn = document.querySelector(".btn");
+console.log(firstBtn);
+console.log("________________________________________________________________________________________________________");
+
+
+// Select the first section element and log it to the console.
+console.log("Select the first section element and log it to the console.");
+const firstSection = document.querySelector(".section");
+console.log(firstSection);
+console.log("________________________________________________________________________________________________________");
+
+
+// Select all button elements with the class btn and log them to the console.
+console.log("Select all button elements with the class btn and log them to the console.");
+const allBtn = document.querySelectorAll(".btn");
+console.log(allBtn);
+console.log("________________________________________________________________________________________________________");
+
+
+// Select all p elements within the container div and log them to the console.
+console.log("Select all p elements within the container div and log them to the console.");
+const allP = document.querySelectorAll(".container p");
+console.log(allP);
+console.log("________________________________________________________________________________________________________");
+
